9888d57d08a8b523c00e4afba89e7d183b0ac718
Helstedxd <fisseerskont@gmail.com>
2014-10-13 08:51:37 +0200

    Klasser

---
+++ FoodPlanner/FoodPlanner/Classes/Meal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodPlanner
+{
+    class Meal
+    {
+        private DateTime time = DateTime.MinValue;
+
+        public Meal()
+        {
+
+        }
+    }
+}
---
+++ FoodPlanner/FoodPlanner/Classes/Storage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodPlanner.Classes
+{
+    abstract class Storage
+    {
+        private string test = null;
+        public Storage(string input)
+        {
+            test = input;
+        }
+    }
+
+    class ShoppingList : Storage
+    {
+        public ShoppingList(string test)
+            : base(test)
+        {
+
+        }
+    }
+
+    class Inventory : Storage
+    {
+        public Inventory(string test)
+            : base(test)
+        {
+
+        }
+    }
+}

15c3b27fdbabbf3629b7384ed8987b6f874b6e65
mathiasvr <mathiasvr@gmail.com>
2014-10-23 15:20:43 +0200

    Inventory Window

---
+++ FoodPlanner/FoodPlanner/InventoryWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodPlanner
+{
+    /// <summary>
+    /// Interaction logic for InventoryWindow.xaml
+    /// </summary>
+    public partial class InventoryWindow : Window
+    {
+        public InventoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource userViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("userViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // userViewSource.Source = [generic data source]
+        }
+    }
+}

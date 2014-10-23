c52b63784205a7f8bbc09848747aad5da95ee603
Helstedxd <fisseerskont@gmail.com>
2014-10-23 10:09:54 +0200

    database

--- FoodPlanner/FoodPlanner/MainWindow.xaml.cs
+++ FoodPlanner/FoodPlanner/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using FoodPlanner.Models;
 
 namespace FoodPlanner
 {
@@ -25,42 +24,5 @@ namespace FoodPlanner
         {
             InitializeComponent();
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            //TODO: remove this 
-            // Test code for displaying data
-            using (var context = new FoodContext())
-            {
-
-                /*var newRecipe = new Recipe("SnickersBar", "noget hej", 300, "whatevers");
-
-                var cf1 = new Food(new Ingredient("Chokolade"), 500);
-                var cf2 = new Food(new Ingredient("Karamel"), 100);
-                var cf3 = new Food(new Ingredient("Nødder"), 10);
-
-                newRecipe.Ingredients.Add(cf1);
-                newRecipe.Ingredients.Add(cf2);
-                newRecipe.Ingredients.Add(cf3);
-
-                context.Recipes.Add(newRecipe);
-                context.SaveChanges();*/
-
-                foreach (Recipe recipe in context.Recipes)
-                {
-                    Console.WriteLine(recipe.Title);
-                    if (recipe.Ingredients != null)
-                    {
-                        foreach (Food food in recipe.Ingredients)
-                        {
-                            Console.WriteLine("  -" + food.Ingredient + " (" + food.Quantity + ")");
-                        }
-                    }
-                }
-
-            }
-        }
-
-
     }
 }

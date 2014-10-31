307bc9cd1c262846863f03ccc3152abc9a2864a1
ChrStephansen <christian.step90@gmail.com>
2014-10-31 12:30:54 +0100

    Slidepannel

---
+++ FoodPlanner/FoodPlanner/UserControlButtonSlide.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodPlanner
+{
+	/// <summary>
+	/// Interaction logic for UserControlButtonSlide.xaml
+	/// </summary>
+	public partial class UserControlButtonSlide : UserControl
+	{
+		public UserControlButtonSlide()
+		{
+			this.InitializeComponent();
+		}
+	}
+}

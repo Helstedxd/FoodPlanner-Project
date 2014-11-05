1af10e295301b3a29effe338117264b0e30c9e96
ChrStephansen <christian.step90@gmail.com>
2014-11-05 09:24:22 +0100

    Pages and slidepannel added

---
+++ FoodPlanner/FoodPlanner/ShoppingListPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodPlanner {
+    /// <summary>
+    /// Interaction logic for ShoppingListPage.xaml
+    /// </summary>
+    public partial class ShoppingListPage : Page {
+        public ShoppingListPage() {
+            InitializeComponent();
+        }
+    }
+}

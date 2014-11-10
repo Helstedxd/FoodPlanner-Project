6bbc4a081bcbb63cbcacffffad79472d18a8e7a1
mathiasvr <mathiasvr@gmail.com>
2014-11-10 10:59:18 +0100

    Moving folders

--- FoodPlanner/FoodPlanner/Helper Classes/RelayCommand.cs
+++ FoodPlanner/FoodPlanner/Helper Classes/RelayCommand.cs
@@ -4,55 +4,40 @@ using System.Windows.Input;
 
 namespace FoodPlanner.Helper_Classes
 {
-    /// <summary>
-    /// A command whose sole purpose is to 
-    /// relay its functionality to other
-    /// objects by invoking delegates. The
-    /// default return value for the CanExecute
-    /// method is 'true'.
-    /// </summary>
+    //http://msdn.microsoft.com/en-us/magazine/dd419663.aspx#id0090030
+
     public class RelayCommand : ICommand
     {
         #region Fields
 
         readonly Action<object> _execute;
-        readonly Predicate<object> _canExecute;        
+        readonly Predicate<object> _canExecute;
 
         #endregion // Fields
 
         #region Constructors
 
-        /// <summary>
-        /// Creates a new command that can always execute.
-        /// </summary>
-        /// <param name="execute">The execution logic.</param>
         public RelayCommand(Action<object> execute)
             : this(execute, null)
         {
         }
 
-        /// <summary>
-        /// Creates a new command.
-        /// </summary>
-        /// <param name="execute">The execution logic.</param>
-        /// <param name="canExecute">The execution status logic.</param>
         public RelayCommand(Action<object> execute, Predicate<object> canExecute)
         {
             if (execute == null)
                 throw new ArgumentNullException("execute");
 
             _execute = execute;
-            _canExecute = canExecute;           
+            _canExecute = canExecute;
         }
-
         #endregion // Constructors
 
         #region ICommand Members
 
         [DebuggerStepThrough]
-        public bool CanExecute(object parameters)
+        public bool CanExecute(object parameter)
         {
-            return _canExecute == null ? true : _canExecute(parameters);
+            return _canExecute == null ? true : _canExecute(parameter);
         }
 
         public event EventHandler CanExecuteChanged
@@ -61,9 +46,9 @@ namespace FoodPlanner.Helper_Classes
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public void Execute(object parameters)
+        public void Execute(object parameter)
         {
-            _execute(parameters);
+            _execute(parameter);
         }
 
         #endregion // ICommand Members
--- FoodPlanner/FoodPlanner/ViewModels/InventoryViewModel.cs
+++ FoodPlanner/FoodPlanner/ViewModels/InventoryViewModel.cs
@@ -5,23 +5,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using FoodPlanner.Models;
+using System.Windows.Input;
+using FoodPlanner.Helper_Classes;
 
 namespace FoodPlanner.ViewModels
 {
 
     public class InventoryViewModel
     {
-
-        public CollectionViewSource InventoryIngredientsCollectionViewSource { get; private set; }
+        private CollectionViewSource _inventoryIngredientsCollectionViewSource;
+        private ICommand _saveInventoryCommand;
 
         public InventoryViewModel()
         {
-            InventoryIngredientsCollectionViewSource = new CollectionViewSource();
-
-            InventoryIngredientsCollectionViewSource.Source = MainWindow.CurrentUser.InventoryIngredients; 
+            InventoryIngredientsCollectionViewSource.Source = MainWindow.CurrentUser.InventoryIngredients;
         }
 
-      
+        public CollectionViewSource InventoryIngredientsCollectionViewSource
+        {
+            get
+            {
+                if (_inventoryIngredientsCollectionViewSource == null)
+                {
+                    _inventoryIngredientsCollectionViewSource = new CollectionViewSource();
+                }
+                return _inventoryIngredientsCollectionViewSource;
+            }
+        }
+
+        public ICommand SaveInventoryCommand
+        {
+            get
+            {
+                if (_saveInventoryCommand == null)
+                {
+                    _saveInventoryCommand = new RelayCommand(p => SaveInventory());
+                }
+
+                return _saveInventoryCommand;
+            }
+        }
+
+        private void SaveInventory()
+        {
+            // Remove unlinked items from the database
+            foreach (var inventoryItem in MainWindow.db.InventoryIngredients.Local.ToList())
+            {
+                if (inventoryItem.Ingredient == null)
+                {
+                    MainWindow.db.InventoryIngredients.Remove(inventoryItem);
+                }
+            }
+
+            MainWindow.db.SaveChanges();
+
+            // Refresh the grids so the database generated values show up. 
+            //this.inventoryIngredientsDataGrid.Items.Refresh();
+            Console.WriteLine("Inventory saved!");
+        }
 
     }
 }

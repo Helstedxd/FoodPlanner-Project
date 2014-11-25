38e9514ebd7d9d46ab66020c52bbc9b6660a695b
Helstedxd <fisseerskont@gmail.com>
2014-11-25 08:54:12 +0100

    opdatering af søgning

--- FoodPlanner/FoodPlanner/ViewModels/RecipeSearchViewModel.cs
+++ FoodPlanner/FoodPlanner/ViewModels/RecipeSearchViewModel.cs
@@ -12,24 +12,6 @@ namespace FoodPlanner.ViewModels
 {
     public class RecipeSearchViewModel
     {
-        private List<inventoryListGroupedByQuantity> inventoryList = (from ii in App.db.InventoryIngredients
-                                                                       where ii.UserID == App.CurrentUser.ID
-                                                                       group ii by ii.IngredientID into iig
-                                                                       select new inventoryListGroupedByQuantity()
-                                                                       {
-                                                                           IngredientID = iig.FirstOrDefault().IngredientID,
-                                                                           Quantity = iig.Sum(i => i.Quantity),
-                                                                           Ingredient = iig.FirstOrDefault().Ingredient,
-                                                                           ExpirationDate = iig.FirstOrDefault().ExpirationDate,
-                                                                           PurchaseDate = iig.FirstOrDefault().PurchaseDate,
-                                                                           User = iig.FirstOrDefault().User,
-                                                                           UserID = iig.FirstOrDefault().UserID
-                                                                       }).ToList();
-
-        private List<int> blacklistedRecipes = (from bl in App.db.BlacklistIngredients
-                                                join ri in App.db.RecipeIngredients on bl.IngredientID equals ri.IngredientID
-                                                select ri.RecipeID).ToList();
-
         public RecipeSearchViewModel()
         {
             this.listOfSearchResults = new ObservableCollection<SearchResults>();
@@ -45,7 +27,10 @@ namespace FoodPlanner.ViewModels
             set
             {
                 _searchText = value;
-                SearchRecipes(_searchText);
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    SearchRecipes(_searchText);
+                }
             }
 
         }
@@ -53,64 +38,36 @@ namespace FoodPlanner.ViewModels
         private void SearchRecipes(string query)
         {
             listOfSearchResults.Clear();
-            List<SearchResults> testtest = new List<SearchResults>();
-
             List<string> searchQuery = query.Split(',').Select(s => s.Trim()).ToList();
 
-            IQueryable<int> recipeIDs = (from ri in App.db.RecipeIngredients
-                                         join i in App.db.Ingredients on ri.IngredientID equals i.ID
-                                         join r in App.db.Recipes on ri.RecipeID equals r.ID
-                                         where searchQuery.Any(s => i.Name.Contains(s)) || searchQuery.Any(s => r.Title.Contains(s))
-                                         group ri by ri.RecipeID into searchRecipeID
-                                         select searchRecipeID.FirstOrDefault().RecipeID);
+            PublicQuerys publicQuerys = new PublicQuerys();
 
-            IQueryable<IGrouping<int, Result>> recipeIngredients = from ri in App.db.RecipeIngredients
-                                                                   join i in App.db.Ingredients on ri.IngredientID equals i.ID
-                                                                   join r in App.db.Recipes on ri.RecipeID equals r.ID
-                                                                   where recipeIDs.Any(rid => rid == ri.RecipeID && blacklistedRecipes.Where(bl => bl == rid).Count() == 0)
-                                                                   group new Result()
-                                                                   {
-                                                                       recipe = ri.Recipe,
-                                                                       ingredient = ri.Ingredient,
-                                                                       quantity = ri.Quantity
-                                                                   } by ri.RecipeID;
+            List<int> recipeIDByIngredients = (from ri in App.db.RecipeIngredients
+                                               join i in App.db.Ingredients on ri.IngredientID equals i.ID
+                                               where searchQuery.Any(s => i.Name.Contains(s))
+                                               select ri.RecipeID).ToList();
 
-            foreach (IGrouping<int, Result> ri in recipeIngredients)
+            List<int> recipeIDByRecipeName = (from ri in App.db.RecipeIngredients
+                                              join r in App.db.Recipes on ri.RecipeID equals r.ID
+                                              where searchQuery.Any(s => r.Title.Contains(s))
+                                              select ri.RecipeID).ToList();
+
+            recipeIDByIngredients.AddRange(recipeIDByRecipeName);
+
+            List<int> recipeIDs = recipeIDByIngredients.Distinct().Except(publicQuerys.blackList).ToList();
+
+            IQueryable<IGrouping<int, Result>> groupedRecipes = publicQuerys.search(recipeIDs);
+
+            List<SearchResults> orderedListOfSearchResults = publicQuerys.addValuesToSearch(groupedRecipes, searchQuery).OrderByDescending(res => res.fullMatch)
+                                                                                                                        .ThenByDescending(res => res.partialMatch)
+                                                                                                                        .ThenByDescending(res => res.getRating)
+                                                                                                                        .ThenByDescending(res => res.prevIngredients)
+                                                                                                                        .ThenByDescending(res => res.recipe.Title)
+                                                                                                                        .ToList();
+
+            foreach (SearchResults sr in orderedListOfSearchResults)
             {
-                Recipe recipe = ri.FirstOrDefault().recipe;
-
-                SearchResults result = new SearchResults(recipe);
-
-                if (searchQuery.Any(s => recipe.Title.ToLower().Contains(s.ToLower())))
-                {
-                    result.keyWordMatch++;
-                }
-
-
-                foreach (Result res in ri)
-                {
-                    result.addIngredient(res.ingredient);
-
-                    if (inventoryList.Where(il => il.IngredientID == res.ingredient.ID).Count() != 0)
-                    {
-                        if (inventoryList.Where(il => il.IngredientID == res.ingredient.ID).First().Quantity >= res.quantity)
-                        {
-                            result.fullMatch++;
-                        }
-                        else
-                        {
-                            result.partialMatch++;
-                        }
-                    }
-
-                    if (searchQuery.Any(s => res.ingredient.Name.ToLower().Contains(s.ToLower())))
-                    {
-                        result.keyWordMatch++;
-                    }
-
-                }
-
-                listOfSearchResults.Add(result);
+                listOfSearchResults.Add(sr);
             }
         }
 

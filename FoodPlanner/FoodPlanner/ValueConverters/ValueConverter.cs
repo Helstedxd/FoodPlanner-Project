d8de19015b6aca6e191d2a9064dc70ea7a2d69d4
mathiasvr <mathiasvr@gmail.com>
2014-11-26 14:13:48 +0100

    Inventory Numeric up/down + Expander + ting

---
+++ FoodPlanner/FoodPlanner/ValueConverters/ValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using FoodPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace FoodPlanner.ValueConverters
+{
+    public class ValueConverter
+    {
+    }
+
+    public class InventoryIngredientCominedQuantityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ReadOnlyObservableCollection<object> col = value as ReadOnlyObservableCollection<object>;
+
+            if (col != null)
+                return col.Sum(o => ((InventoryIngredient)o).Quantity);
+
+            throw new NotSupportedException("Converter only supports items of type ReadOnlyObservableCollection<object>");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Do the conversion from visibility to bool
+            throw new NotImplementedException();
+        }
+    }
+
+
+}

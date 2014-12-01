a78c3e98e01bc06ab5b62ce9e156127768eeaccd
mathiasvr <mathiasvr@gmail.com>
2014-12-01 11:17:00 +0100

    Inventory page sorting stuff

--- FoodPlanner/FoodPlanner/ViewModels/InventoryViewModel.cs
+++ FoodPlanner/FoodPlanner/ViewModels/InventoryViewModel.cs
@@ -9,6 +9,7 @@ using FoodPlanner.Models;
 using System.Windows.Input;
 using System.Collections.ObjectModel;
 using MvvmFoundation.Wpf;
+using System.ComponentModel;
 
 namespace FoodPlanner.ViewModels
 {
@@ -20,22 +21,56 @@ namespace FoodPlanner.ViewModels
 
         private ICommand _saveInventoryCommand;
         private ICommand _addIngredientToInventory;
+        private int _selectedSortIndex;
 
         #endregion
 
         public InventoryViewModel()
         {
-            InventoryIngredients =
-                // Clone the collection to avoid wierd error..
-                new ObservableCollection<InventoryIngredient>(App.CurrentUser.InventoryIngredients);
+            // Clone the collection to avoid wierd error..
+            InventoryIngredients = new ObservableCollection<InventoryIngredient>(App.CurrentUser.InventoryIngredients);
 
-                // Add Grouping View
-                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(InventoryIngredients);
-                PropertyGroupDescription groupDescription = new PropertyGroupDescription("Ingredient");
-                view.GroupDescriptions.Add(groupDescription);
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(InventoryIngredients);
+
+            // Group by Ingredient 
+            PropertyGroupDescription groupDescription = new PropertyGroupDescription("Ingredient");
+            view.GroupDescriptions.Add(groupDescription);
+
+            // Sort Descriptions and Names to display in combobox
+            //TODO: this should probably be key-value pairs or something, instead of two arrays.
+            SortDescriptions = new List<SortDescription>() {
+                new SortDescription("Ingredient.Name", ListSortDirection.Ascending),
+                new SortDescription("ExpirationDate", ListSortDirection.Ascending),
+                new SortDescription("Quantity", ListSortDirection.Descending)
+            };
+
+            SortDescriptionNames = new ObservableCollection<string>() {
+                "Name",
+                "Expiration Date",
+                "Quantity"
+            };
+
+            SelectedSortIndex = 0;
+
         }
 
         #region Properties
+
+        public List<SortDescription> SortDescriptions { get; private set; }
+        public ObservableCollection<string> SortDescriptionNames { get; private set; }
+
+        public int SelectedSortIndex
+        {
+            get { return _selectedSortIndex; }
+            set
+            {
+                _selectedSortIndex = value;
+                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(InventoryIngredients);
+                foreach (SortDescription sr in SortDescriptions)
+                    view.SortDescriptions.Remove(sr);
+                view.SortDescriptions.Add(SortDescriptions[_selectedSortIndex]);
+            }
+        }
 
         //public IEnumerable<IGrouping<int, InventoryIngredient>> InventoryIngredients { get; set; }
         public ObservableCollection<InventoryIngredient> InventoryIngredients { get; set; }
@@ -76,22 +111,26 @@ namespace FoodPlanner.ViewModels
         {
             if (ingredient != null)
             {
-                App.CurrentUser.InventoryIngredients.Add(new InventoryIngredient(ingredient, 1));
+                InventoryIngredient newInventoryIngredient = new InventoryIngredient(ingredient, 1);
+                App.CurrentUser.InventoryIngredients.Add(newInventoryIngredient);
+                //TODO: InventoryIngredients is a copy of App.CurrentUser.InventoryIngredients
+                // It would be better if we only needed to update one of them...
+                InventoryIngredients.Add(newInventoryIngredient);
             }
         }
 
         private void SaveInventory()
         {
             //TODO: stuff (Inventory is not saved if you leave the page..)
-            List<InventoryIngredient> currentUserInventory = App.CurrentUser.InventoryIngredients.ToList();
+            // List<InventoryIngredient> currentUserInventory = App.CurrentUser.InventoryIngredients.ToList();
 
-            foreach (InventoryIngredient ii in App.db.InventoryIngredients.Where(ii => ii.UserID == App.CurrentUser.ID))
+            /*foreach (InventoryIngredient ii in App.db.InventoryIngredients.Where(ii => ii.UserID == App.CurrentUser.ID))
             {
                 if (currentUserInventory.Where(ci => ci.ID == ii.ID).Count() == 0)
                 {
                     App.db.InventoryIngredients.Remove(ii);
                 }
-            }
+            }*/
 
             App.db.SaveChanges();
         }

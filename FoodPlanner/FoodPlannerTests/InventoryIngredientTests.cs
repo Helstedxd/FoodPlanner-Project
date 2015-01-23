263b15921d0e801926e7b24012a8226167966632
GideonBlegmand <Gidde_94@hotmail.com>
2015-01-23 19:04:55 +0100

    tests setup og to simple unit tests

---
+++ FoodPlanner/FoodPlannerTests/InventoryIngredientTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodPlanner.Models;
+
+namespace FoodPlannerTests {
+    [TestClass]
+    public class InventoryIngredientTests {
+
+        [TestMethod]
+        public void PurchaseDate_AutoSetInConstructor_SetToNow() {
+            //Arrange
+            InventoryIngredient inventoryIngredientTest = new InventoryIngredient(new Ingredient(), 750);
+            DateTime expectedPurchaseDate = DateTime.Now;
+
+            //act - blank because the property is set automaticly in the constructor
+
+            //assert
+            Assert.AreEqual(expectedPurchaseDate, inventoryIngredientTest.PurchaseDate);
+        }
+
+        [TestMethod]
+        public void ExpirationDate_AutoSetInConstructor_SetTo7DaysAhead() {
+            DateTime expectedExpirationDate = DateTime.Now.AddDays(7);
+            InventoryIngredient inventoryIngredientTest = new InventoryIngredient(new Ingredient(), 750);
+
+            //act - blank because the property is set automaticly in the constructor
+
+            //assert
+            Assert.AreEqual(expectedExpirationDate, inventoryIngredientTest.ExpirationDate);
+        }
+    }
+}

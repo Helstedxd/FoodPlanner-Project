b054c074093964f0112d4a412bc21dfe2d24365e
GideonBlegmand <Gidde_94@hotmail.com>
2015-01-25 16:45:33 +0100

    unit tests

--- FoodPlanner/FoodPlannerTests/InventoryViewModelTests.cs
+++ FoodPlanner/FoodPlannerTests/InventoryViewModelTests.cs
@@ -2,25 +2,52 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FoodPlanner.Models;
 using FoodPlanner.ViewModels;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace FoodPlannerTests {
     [TestClass]
     public class InventoryViewModelTests {
         [TestMethod]
-        public void AddInventoryIngredient_CorrectIngrAdded_IngrAdded() {
+        public void AddInventoryIngredient_CorrectIngrAdded_InventoryUpdated() {
             //arrange
             Ingredient expectedNewInvenIngredient = new Ingredient();
-            expectedNewInvenIngredient.Name = "Milk";
+            InventoryIngredient inventoryIngredientTest = new InventoryIngredient(expectedNewInvenIngredient, 1);
+            InventoryViewModel invVM = new InventoryViewModel();
+            bool ingredientAdded;
 
             //act
+            invVM.AddIngredientToInventory(expectedNewInvenIngredient, 1);
             
+            if (FoodPlanner.App.CurrentUser.InventoryIngredients.Contains(inventoryIngredientTest)) {
+                ingredientAdded = true;
+            } else {
+                ingredientAdded = false;
+            }
 
             //assert
+            Assert.IsTrue(ingredientAdded);
         }
 
         [TestMethod]
-        public void AddInventoryIngredient_With1Quantity_CorrectQuan() {
+        public void RemoveInventoryIngredient_CorrectRemoved_InventoryUpdated() {
+            //arrange
+            InventoryViewModel invVM = new InventoryViewModel();
+            Ingredient ingredient = new Ingredient();
+            InventoryIngredient inventoryIngredient = new InventoryIngredient();
+            inventoryIngredient.Ingredient = ingredient;
+            bool inventoryIngredientRemoved = false;
 
+            //act
+            invVM.AddIngredientToInventory(ingredient);
+            invVM.RemoveIngredientFromInventory(inventoryIngredient);
+
+            if (!FoodPlanner.App.CurrentUser.InventoryIngredients.Contains(inventoryIngredient)) {
+                inventoryIngredientRemoved = true;
+            }
+            //assert
+            Assert.IsTrue(inventoryIngredientRemoved);
         }
     }
 }
